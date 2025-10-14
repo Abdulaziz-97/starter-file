4c1577c296a0e09151425f2e4b76e643c1ce312b
Abudlaziz-97 <3ziz9788@gmail.com>
2025-10-15 01:44:56 +0300

    data_ingestion_ finshed

--- ingestion.py
+++ ingestion.py
@@ -19,8 +19,31 @@ output_folder_path = config['output_folder_path']
 #############Function for data ingestion
 def merge_multiple_dataframe():
     #check for datasets, compile them together, and write to an output file
+    if not os.path.exists(output_folder_path):
+        os.mkdir(output_folder_path)
+    
+    files = []
+    for file in os.listdir(input_folder_path):
+        if file.endswith('.csv'):
+            files.append(file)
+    
+    df_list = []
+    for filename in files:
+        filepath = os.path.join(input_folder_path, filename)
+        df = pd.read_csv(filepath)
+        df_list.append(df)
+    print(df_list[0].shape)
+    print(len(df_list))
+    print("names", df_list[0].columns)
 
+    final_df = pd.concat(df_list)
+    final_df.drop_duplicates(inplace=True)
+    print(final_df.shape)
+    print("names", final_df.columns)
+    final_df.to_csv(os.path.join(output_folder_path, 'finaldata.csv'), index=False)
 
+    with open(os.path.join(output_folder_path, 'ingestedfiles.txt'), 'w') as f:
+        f.write(files + "\n")
 
-if __name__ == '__main__':
+ if __name__ == '__main__':
     merge_multiple_dataframe()
